86b77864947f69b99af654c9028681a7d6b59dbc
kiranatmakuri516 <104350125+kiranatmakuri516@users.noreply.github.com>
2023-02-07 18:22:11 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,101 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    unordered_map<int,int>mp;
+    while(head1)
+    {
+        mp[head1->data]++;
+        head1=head1->next;
+    }
+    while(head2)
+    {
+        if(mp[head2->data]>0)
+        cout<<head2->data<<" ";
+        mp[head2->data]--;;
+        head2=head2->next;
+    }
+    return head1;
+}
