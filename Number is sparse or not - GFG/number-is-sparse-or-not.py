03f8a5512de061565ec012b06b5fc8ba65a08459
kiranatmakuri516 <104350125+kiranatmakuri516@users.noreply.github.com>
2023-03-02 09:55:48 +0530

    Added solution - LeetHub

---
+++ Number is sparse or not - GFG/number-is-sparse-or-not.py
@@ -0,0 +1,40 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to check if the number is sparse or not.
+    def isSparse(self,n):
+        k='{0:032b}'.format(n)
+        if '11' in k:
+            return 0
+        else:
+            return 1
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        if ob.isSparse(n):
+            print("1")
+        else:
+            print("0")
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
